de6b620ab2b0d9dc2d87dcbd84bbad3221ab8215
madsmads4 <madsolsen213@gmail.com>
2025-11-10 10:43:03 +0100

    Update CommunitySection.vue

--- Faraos-Cigarer/src/components/CommunitySection.vue
+++ Faraos-Cigarer/src/components/CommunitySection.vue
@@ -1,6 +1,12 @@
 <script setup></script>
 
-<template></template>
+<template>
+  <div class="community-section">
+    <div class="community-section__klub-faraos"></div>
+    <div class="community-section__e-label"></div>
+    <div class="community-section__news"></div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
 @import "../assets/main.scss";
