94c48e1410424672103be422496883c281bf97ca
madsmads4 <madsolsen213@gmail.com>
2025-11-11 09:53:15 +0100

    push

--- Faraos-Cigarer/src/components/SocialsSection.vue
+++ Faraos-Cigarer/src/components/SocialsSection.vue
@@ -38,6 +38,34 @@
   height: 5rem;
   background-color: $color-pharaos-gold;
   color: $color-newspaper-white;
+  -webkit-text-stroke: 0.1rem $color-anubis-black;
+  font-family: $font-boogaloo;
+  font-size: 2.44rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.socials-section__tiktok {
+  width: 100%;
+  height: 5rem;
+  background-color: $color-anubis-black;
+  color: $color-pharaos-gold;
+  font-family: $font-boogaloo;
+  font-size: 2.44rem;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.socials-section__twitter {
+  width: 100%;
+  height: 5rem;
+  background-color: $color-pharaos-gold;
+  color: $color-newspaper-white;
+  -webkit-text-stroke: 0.1rem $color-anubis-black;
   font-family: $font-boogaloo;
   font-size: 2.44rem;
   cursor: pointer;
