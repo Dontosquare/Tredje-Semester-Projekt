93abaa62e4f0ec8a5941fe335863e9067507f36d
madsmads4 <madsolsen213@gmail.com>
2025-11-11 13:07:40 +0100

    Update ContactComponent.vue

--- Faraos-Cigarer/src/components/ContactComponent.vue
+++ Faraos-Cigarer/src/components/ContactComponent.vue
@@ -2,17 +2,23 @@
 
 <template>
   <div class="aabningstider">
-    <p class="aabningstider__titel"></p>
-    <p class="aabningstider__text"></p>
+    <p class="aabningstider__titel">ÅBNINGSTIDER</p>
+    <p class="aabningstider__text">
+      Hverdage: Mandag - Fredag: 10:00 - 18:00 Weekendtider: Lørdag - 10:00 -
+      17:00 Søndag - 11:00 - 16:00 *Faraos Cigarer holder dog søndagslukket, når
+      søndag falder på en helligdag.
+    </p>
   </div>
 
   <div class="kontakt">
-    <p class="kontakt__titel"></p>
-    <p class="kontakt__text"></p>
+    <p class="kontakt__titel">KONTAKT</p>
+    <p class="kontakt__text">
+      Faraos Cigarer - Odense Klostervej 3,5000 Odense C Telefon: +45 65 91 97
+      01 E-mail: Odense@faraos.dk
+    </p>
   </div>
 
   <div class="lukke-dage">
-    <p class="lukke-dage__titel"></p>
     <p class="lukke-dage__text"></p>
   </div>
 </template>
