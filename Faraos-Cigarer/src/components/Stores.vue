4a54a5e5ec2d446d6e5827c19489e6fa5b8f0b42
SofiePedersen <sofie@gefiber.dk>
2025-11-12 14:19:36 +0100

    Helped fix css

--- Faraos-Cigarer/src/components/Stores.vue
+++ Faraos-Cigarer/src/components/Stores.vue
@@ -10,17 +10,17 @@ const storeMapSections = ref([
     id: 'jylland',
     title: 'JYLLAND',
     items: [
-      { id: 'j1', title: 'Find alle de lokale butikker på Jylland!', url: '#'},
-      { id: 'j2', title: 'AARHUS GAMES', url: '#'},
-      { id: 'j3', title: 'AARHUS COMICS', url: '#'},
+      { id: 'j1', title: 'Find alle de lokale butikker på Jylland!', url: '#' },
+      { id: 'j2', title: 'Aarhus games', url: '#' },
+      { id: 'j3', title: 'Aarhus comics', url: '#' },
     ],
   },
   {
     id: 'fyn',
     title: 'FYN',
     items: [
-      { id: 'f1', title: 'Find alle de lokale butikker på Fyn!', url: '#'},
-      { id: 'f2', title: 'ODENSE', url: '/information' },
+      { id: 'f1', title: 'Find alle de lokale butikker på Fyn!', url: '#' },
+      { id: 'f2', title: 'Odense', url: '/information' },
     ],
   },
   {
@@ -28,9 +28,9 @@ const storeMapSections = ref([
     title: 'SJÆLLAND',
     items: [
       { id: 's1', title: 'Find alle de lokale butikker på Sjælland!', url: '#' },
-      { id: 's2', title: 'KØBENHAVN COMICS & POTTER', url: '#'}, 
-      { id: 's3', title: 'KØBENHAVN BRÆT- & ROLLESPIL', url: '#'},
-      { id: 's4', title: 'KØBENHAVN FIGURSPIL', url: '#'},
+      { id: 's2', title: 'København comics & potter', url: '#' },
+      { id: 's3', title: 'København bræt- & rollespil', url: '#' },
+      { id: 's4', title: 'København figurspil', url: '#' },
     ],
   },
 ]);
@@ -44,50 +44,89 @@ function toggleSection(id) {
 
 
 <template>
-  <iframe
-    src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d577050.2589892185!2d10.37101225287178!3d55.60022845645195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sFaraos%20Cigarer!5e0!3m2!1sda!2sdk!4v1762941964703!5m2!1sda!2sdk"
-    width="100%" height="500rem" style="border:0;" allowfullscreen="" loading="lazy"
-    referrerpolicy="no-referrer-when-downgrade"></iframe>
-  <h2>HVOR VIL DU HEN?</h2>
+  <div class="stores-wrapper">
+    <nav class="breadcrumbs" aria-label="Navigations brødkrummer">
+      <RouterLink class="breadcrumbs__text" to="/" aria-label="Navigation til forsiden">Forside</RouterLink>
+      <p class="breadcrumbs__text">></p>
+      <RouterLink class="breadcrumbs__text" to="/Butikker" aria-label="Navigation til oversigten over butikker">Butikker
+      </RouterLink>
+      <p class="breadcrumbs__text">></p>
+      <RouterLink class="breadcrumbs__text" to="/Information" aria-label="Navigation til Odense butik">Odense
+      </RouterLink>
+    </nav>
 
-  <div v-for="section in storeMapSections" :key="section.id" class="butikker__sektion" @click="toggleMenu" aria-label="landsdele sektioner">
-    <button class="butikker__sektion--knap" @click="toggleSection(section.id)" aria-label="åben/luk felt">
-      {{ section.title }}
-      <FontAwesomeIcon :icon="faAngleDown" :class="{'rotate-180': openSection === section.id}" class="butikker__ikon" aria-label="åben/luk pil"/>
-    </button>
+    <div class="map-container" aria-label="Google maps over butikkens lokation">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d577050.2589892185!2d10.37101225287178!3d55.60022845645195!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1sFaraos%20Cigarer!5e0!3m2!1sda!2sdk!4v1762941964703!5m2!1sda!2sdk"
+          width="100%" height="500rem" style="border:0;" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
 
-    <transition name="slide-fade">
-    <div class="listitem__controls">
-      <ul v-if="openSection === section.id" class="butikker__sektion__boks" aria-label="informations boks">
-        <li v-for="item in section.items" :key="item.id" class="butikker__sektion__listitem">
-          <RouterLink :to="item.url" class="butikker__links">{{ item.title }}</RouterLink>
-        </li>
-      </ul>
-    </div>  
-    </transition>
+    <h2>HVOR VIL DU HEN?</h2>
+
+    <div v-for="section in storeMapSections" :key="section.id" class="butikker__sektion" @click="toggleMenu"
+      aria-label="landsdele sektioner">
+      <button class="butikker__sektion--knap" @click="toggleSection(section.id)" aria-label="åben/luk felt">
+        {{ section.title }}
+        <FontAwesomeIcon :icon="faAngleDown" :class="{ 'rotate-180': openSection === section.id }" class="butikker__ikon"
+          aria-label="åben/luk pil" />
+      </button>
+
+      <transition name="slide-fade">
+        <div class="listitem__controls">
+          <ul v-if="openSection === section.id" class="butikker__sektion__boks" aria-label="informations boks">
+            <li v-for="item in section.items" :key="item.id" class="butikker__sektion__listitem">
+              <RouterLink :to="item.url" class="butikker__links">{{ item.title }}</RouterLink>
+            </li>
+          </ul>
+        </div>
+      </transition>
+    </div>
   </div>
-
- 
 </template>
 
 <style lang="scss" scoped>
 @import "../assets/main.scss";
 
-.butikker__kort {
-  width: 100%;
-  height: auto;
+.stores-wrapper {
+  padding: 2rem;
+  padding-top: 0.5rem;
+}
+
+.breadcrumbs {
+  display: flex;
+  justify-content: flex-start;
+  padding:0rem 0rem 2rem 0rem;
+
+  p {
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+  }
+}
+
+.breadcrumbs__text {
+  font-size: 1rem;
+  color: $color-anubis-black;
+  text-decoration: none;
+  font-family: $font-play;
+}
+
+.breadcrumbs__text:hover {
+  color: #3396E6;
 }
 
 .butikker__sektion {
   display: flex;
   flex-direction: column;
-  margin-bottom: 1rem;
+  margin-bottom: 1.5rem;
   justify-content: center;
   align-items: center;
-  margin-left: 20%;
-  margin-right: 20%;
   color: $color-pharaos-gold;
   background-color: $color-anubis-black;
+}
+
+.butikker__sektion:last-of-type {
+  margin-bottom: 0;
 }
 
 .butikker__ikon {
@@ -114,39 +153,34 @@ function toggleSection(id) {
   justify-content: center;
   justify-content: space-between;
   align-items: center;
-  gap: 0.5rem;
-  font-family: "Boogaloo";
+  font-family: $font-boogaloo;
   cursor: pointer;
 }
 
 .listitem__controls {
   width: 100%;
-  display: flex;
-  justify-content: start;
-  padding: 0;
-  align-items: left;
+  background-color: $color-pharaos-gold;
 }
 
 .butikker__sektion__boks {
   width: 100%;
-  padding: 0;
+  padding: 1.5rem;
   color: $color-anubis-black;
-  background-color: $color-pharaos-gold;
 }
 
 .butikker__links {
-  justify-content: center;
   text-decoration: none;
-  font-family: "play";
+  font-family: $font-play;
   color: $color-anubis-black;
 }
 
-
 .butikker__sektion__listitem {
-  margin: 3rem 0;
-  justify-content: center;
+  margin-left: 1rem;
 }
 
-
-
+.butikker__sektion__listitem:first-of-type {
+  list-style-type: none;
+  margin: 0;
+  margin-bottom: 1rem;
+}
 </style>
